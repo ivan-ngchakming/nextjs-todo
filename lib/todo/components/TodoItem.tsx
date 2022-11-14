2b2c5426e3f652f592c5bd8ac14282e83c7756af
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-11-14 18:11:57 -0500

    render todo items

--- lib/todo/components/TodoItem.tsx
+++ lib/todo/components/TodoItem.tsx
@@ -2,12 +2,13 @@ import { CheckBox } from "@mui/icons-material";
 import { Box, IconButton, Typography } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Todo } from "../type";
 
-const TodoItem = ({ title }: { title: string }) => {
+const TodoItem = ({ item }: { item: Todo }) => {
   return (
     <Box display="flex">
       <CheckBox />
-      <Typography sx={{ ml: 2, flexGrow: 1 }}>{title}</Typography>
+      <Typography sx={{ ml: 2, flexGrow: 1 }}>{item.content}</Typography>
       <IconButton>
         <EditIcon />
       </IconButton>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Navbar } from "../lib/shared/components";
+import { NewItemForm, TodoItem } from "../lib/todo/components";
+import { useTodo } from "../lib/todo/contexts/TodoContext";
 
 export default function Home() {
+  const { items } = useTodo();
+
   return (
     <Box>
       <Navbar />
+      <Container maxWidth="sm" sx={{ mt: 4 }}>
+        <NewItemForm />
+        <Box mt={6}>
+          {items.map((item) => (
+            <TodoItem key={item.id} item={item} />
+          ))}
+        </Box>
+      </Container>
     </Box>
   );
 }
